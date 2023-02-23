89c40494fcad275bc1b6cf3721b105f1d0227271
ktadique <117470787+ktadique@users.noreply.github.com>
2023-02-23 01:01:15 +0000

    began working on index and MD script files

--- index.js
+++ index.js
@@ -4,13 +4,69 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    message: "Please enter your name:",
+    name: "name",
+  },
+  {
+    type: "input",
+    message: "Please enter your GitHub username:",
+    name: "username",
+  },
+  {
+    type: "input",
+    message: "Please enter your email:",
+    name: "email",
+  },
+  {
+    type: "input",
+    message: "Please enter the title of your project:",
+    name: "projectTitle",
+  },
+  {
+    type: "input",
+    message: "Please give your project a short description:",
+    name: "projectDesc",
+  },
+  {
+    type: "list",
+    message: "Please select a project license:",
+    name: "projectLicense",
+    choices: ["MIT", "GNU GPL v3", "Mozilla Public License 2.0", "No License"],
+  },
+  {
+    type: "input",
+    message: "Please explain how the project will be installed:",
+    name: "projectInstall",
+  },
+  {
+    type: "input",
+    message: "Please explain how to use the project:",
+    name: "projectUsage",
+  },
+  {
+    type: "input",
+    message: "Please explain how users may contribute to the project:",
+    name: "projectContribute",
+  },
+  {
+    type: "input",
+    message: "Please explain how users may test the project:",
+    name: "projectTest",
+  },
+];
 
 // function to write README file
 function writeToFile(fileName, data) {}
 
 // function to initialize program
-function init() {}
+function init() {
+  inquirer.prompt(questions).then((data) => {
+    console.log(data);
+  });
+}
 
 // function call to initialize program
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,34 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  let licenseBadge = "";
+  const chosenLicense = data.projectLicense;
 
-`;
+  //License Badges
+  let licenseText = "";
+  const licenseMIT = "";
+  const licenseGNU = "";
+  const licenseMozilla = "";
+
+  switch (chosenLicense) {
+    case "MIT":
+      licenseBadge = licenseMIT;
+      licenseText = chosenLicense;
+      break;
+    case "GNU GPL v3":
+      licenseBadge = licenseGNU;
+      licenseText = chosenLicense;
+      break;
+    case "Mozilla Public License 2.0":
+      licenseBadge = licenseMozilla;
+      licenseText = chosenLicense;
+      break;
+    case "No License":
+      licenseText = "This project has no License";
+      break;
+    default:
+      console.log("Please select one of the options");
+      break;
+  }
 }
 
 module.exports = generateMarkdown;
